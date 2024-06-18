9fa6aa13e5277623847792e7f231da5da2ca992f
Mustafiz <hilaly.mustafiz@hotmail.com>
2024-06-18 15:52:41 -0500

    Optimize the code

--- src/main/java/MaxConsecutiveOnes485.java
+++ src/main/java/MaxConsecutiveOnes485.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class MaxConsecutiveOnes485 {
 
     public static int findMaxConsecutiveOnes(int[] nums) {
@@ -6,17 +9,31 @@ public class MaxConsecutiveOnes485 {
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) {
-                max = Math.max(count, max);
+                max = max(count, max);
                 count = 0;
             }
             else count++;
         }
 
-        return Math.max(count, max);
+        return max(count, max);
+    }
+
+    public static int max(int a, int b) {
+        return a >= b ? a : b;
+    }
+
+    public static int findMaxConsecutiveOnesUsingStreamAPI(int[] nums) {
+        return Arrays.stream(Arrays.stream(nums)
+                        .mapToObj(String::valueOf)
+                        .collect(Collectors.joining(""))
+                        .split("0"))
+                .mapToInt(String::length)
+                .max()
+                .orElse(0);
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,1,1,1,1,1};
+        int[] nums = {1,1,0,1,1,1,0,1,0};
 
         int result = findMaxConsecutiveOnes(nums);
 
